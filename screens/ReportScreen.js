a38d17ad1509b6682b8bd03bac9d5bffd69e576c
CarinaLR <carinalr84@gmail.com>
2019-12-17 12:30:11 -0500

    added navigation

--- screens/ReportScreen.js
+++ screens/ReportScreen.js
@@ -1,10 +1,43 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 
 export default function ReportScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.getmefit}>Get Me Fit!</Text>
+      <View style={styles.text}>
+        <Text style={styles.getmefit}>Get Me Fit!</Text>
+        <Text>Here is your meal plan for this week!</Text>
+      </View>
+      <View style={styles.images}>
+        <Image
+          source={{
+            uri:
+              'https://static.onecms.io/wp-content/uploads/sites/44/2019/07/27114300/oatmeal.jpg',
+          }}
+          style={{ width: 100, height: 100 }}
+        />
+        <Image
+          source={{
+            uri:
+              'https://nutritiouslife.com/wp-content/uploads/2015/07/healthy-snack-ideas.jpg',
+          }}
+          style={{ width: 100, height: 100 }}
+        />
+        <Image
+          source={{
+            uri:
+              'https://thegirlonbloor.com/wp-content/uploads/2017/12/Korean-Chicken-Meal-Prep-Bowls-6.jpg',
+          }}
+          style={{ width: 100, height: 100 }}
+        />
+        <Image
+          source={{
+            uri:
+              'https://cdn-image.realsimple.com/sites/default/files/styles/rs_medium_image/public/tilapia-mango.jpg?itok=qSOfthe5',
+          }}
+          style={{ width: 100, height: 100 }}
+        />
+      </View>
     </View>
   );
 }
@@ -16,11 +49,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-around',
   },
+  text: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  images: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
   getmefit: {
-    padding: 30,
+    padding: 10,
     fontSize: 50,
     textAlign: 'center',
     color: 'orange',
-    margin: 10,
+    margin: 5,
   },
 });
--- screens/UserInfoScreen.js
+++ screens/UserInfoScreen.js
@@ -1,6 +1,5 @@
-import * as WebBrowser from 'expo-web-browser';
-import React, { useState } from 'react';
-import { StyleSheet, View, Image, Button, FlatList, Text } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, Image, FlatList, Text, Button } from 'react-native';
 
 import GoalItem from '../components/GoalItem';
 import GoalInput from '../components/GoalInput';
@@ -9,51 +8,16 @@ class UserInfoScreen extends React.Component {
   constructor() {
     super();
   }
-  // const [weightLossGoals, setWeightLossGoals] = useState([]);
-
-  // const addGoalHandler = goalTitle => {
-  //   setWeightLossGoals(currentGoals => [
-  //     ...currentGoals,
-  //     { key: Math.random().toString(), value: goalTitle },
-  //   ]);
-  // };
-
-  // const removeGoalHandler = goalId => {
-  //   setWeightLossGoals(currentGoals => {
-  //     return currentGoals.filter(goal => goal.id !== goalId);
-  //   });
-  // };
-
-  // if (__DEV__) {
-  //   const learnMoreButton = (
-  //     <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-  //       Learn more
-  //     </Text>
-  //   );
 
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.getmefit}>Get Me Fit!</Text>
-        <Text style={styles.welcome}>
-          Based on how much weight you would like to lose we can provide you a
-          weekly meal plan that will help you to reach your goal!
-        </Text>
-        {learnMoreButton}
-        <View style={styles.screen}>
-          <GoalInput onAddGoal={addGoalHandler} />
-          <FlatList
-            keyExtractor={(item, index) => item.key}
-            data={weightLossGoals}
-            renderItem={itemData => (
-              <GoalItem
-                id={itemData.item.id}
-                onDelete={removeGoalHandler}
-                title={itemData.item.value}
-              />
-            )}
-          />
-        </View>
+        <Button
+          title="Continue"
+          style={styles.continue}
+          onPress={() => this.props.navigation.navigate('Report')}
+        />
         <Image
           source={{
             uri:
@@ -64,12 +28,6 @@ class UserInfoScreen extends React.Component {
       </View>
     );
   }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync(
-    'https://www.nhlbi.nih.gov/health/educational/lose_wt/eat/calories.htm'
-  );
 }
 
 export default UserInfoScreen;
@@ -102,5 +60,13 @@ const styles = StyleSheet.create({
   helpLinkText: {
     fontSize: 14,
     color: '#2e78b7',
+  },
+  continue: {
+    color: '#B0C4DE',
+    height: 15,
+    borderRadius: 12,
+    alignItems: 'flex-end',
+    paddingBottom: 5,
+    flex: 1,
   },
 });
